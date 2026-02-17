3ebf11169143dfe9be21777e51d933a216a059e5
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-02-17 07:58:56 +0330

    splt layout in multiple partial view

---
+++ BestShop.Web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestShop.Web.Controllers;
+
+public class HomeController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+
+	public IActionResult AboutUs()
+	{
+		return View();
+	}
+
+
+	public IActionResult ContactUs()
+	{
+		return View();
+	}
+
+}
