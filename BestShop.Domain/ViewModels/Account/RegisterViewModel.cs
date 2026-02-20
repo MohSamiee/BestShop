df48c4a94ad85fb43bd708173a9a93a02efc932f
Mohsen Samiee <Mo.Samiee@outlook.com>
2026-02-21 03:14:24 +0330

    Works on register

---
+++ BestShop.Domain/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+using BestShop.Common.Resources;
+
+namespace BestShop.Domain.ViewModels.Account;
+public class RegisterViewModel
+{
+	[Display(Name = "UserName", ResourceType = typeof(PropertyDictionary))]
+	[MaxLength(100, ErrorMessageResourceName = "GnMaxLengthErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	[Required(ErrorMessageResourceName = "GnRequiredErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	public string UserName { get; set; }
+
+
+	[Display(Name = "Email", ResourceType = typeof(PropertyDictionary))]
+	[MaxLength(100, ErrorMessageResourceName = "GnMaxLengthErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	[Required(ErrorMessageResourceName = "GnRequiredErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	[DataType(DataType.EmailAddress, ErrorMessageResourceName = "GnEmailFormatErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	public string Email { get; set; }
+
+
+	[Display(Name = "Password", ResourceType = typeof(PropertyDictionary))]
+	[Required(ErrorMessageResourceName = "GnRequiredErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	[DataType(DataType.Password)]
+	public string Password { get; set; }
+
+
+	[Display(Name = "RePassword", ResourceType = typeof(PropertyDictionary))]
+	[Required(ErrorMessageResourceName = "GnRequiredErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	[DataType(DataType.Password)]
+	[Compare(nameof(Password), ErrorMessageResourceName = "ComparePasswordErrorMessage", ErrorMessageResourceType = typeof(PropertyDictionary))]
+	public string RePassword { get; set; }
+
+}
---
+++ BestShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using BestShop.Domain.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestShop.Web.Controllers;
+public class AccountController : Controller
+{
+	#region Register
+	[HttpGet("/Register")]
+	public IActionResult Register()
+	{
+		return View();
+	}
+
+	[HttpPost("/Register")]
+	[ValidateAntiForgeryToken]
+	public IActionResult Register(RegisterViewModel vm)
+	{
+		return View();
+	}
+
+	#endregion Register
+}
